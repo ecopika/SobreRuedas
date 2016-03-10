927e96121095bbe015554a571d5616b04f940db8
ecopika <horitzonts@gmail.com>
2016-03-10 13:33:19 +0100

    Implementació Slick 2D -AE

--- app/src/main/java/edu/ub/pis2016/dperezgu12alumnes/sobreruedas/MenuActivity.java
+++ app/src/main/java/edu/ub/pis2016/dperezgu12alumnes/sobreruedas/MenuActivity.java
@@ -3,11 +3,14 @@ package edu.ub.pis2016.dperezgu12alumnes.sobreruedas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MenuActivity extends AppCompatActivity {
+import org.newdawn.slick.SlickActivity;
+
+public class MenuActivity extends SlickActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        //setContentView(R.layout.activity_menu);
+        start(newGame(),850,480);
     }
 }
