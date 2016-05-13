791f37997cbaa21181ff02aea6f739d40801fab5
JESUS ROMARIO ACOSTA CHOCA <jacostch7.alumnes@ub052229.mat.ub.edu>
2016-05-13 13:32:42 +0200

    Modificacions Personatge

--- app/src/main/java/edu/ub/pis2016/dperezgu12alumnes/sobreruedas/joc/Model/objectesJoc/PCadiraRodes.java
+++ app/src/main/java/edu/ub/pis2016/dperezgu12alumnes/sobreruedas/joc/Model/objectesJoc/PCadiraRodes.java
@@ -6,7 +6,7 @@ import android.content.Context;
  * Created by ecopika on 22/03/16.
  */
 public class PCadiraRodes extends Personatge {
-    public PCadiraRodes(String nom, int edat, String colorPref, String animalPref, String menjarPref, String hobby, String idol, String pasions, String habits, String noSoporta, String frase, String somni, String nomImatge,Context cnt, String nomGif) {
+    public PCadiraRodes(String nom, int edat, String colorPref, String menjarPref, String hobby, String idol, String animalPref, String pasions, String habits, String noSoporta, String frase, String somni, String nomImatge,Context cnt, String nomGif) {
         super(nom, edat, colorPref, animalPref, menjarPref, hobby, idol, pasions, habits, noSoporta, frase, somni,nomImatge,cnt,nomGif);
 
     }
